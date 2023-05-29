19878c039c246a1484b2d53612d7c39253839db4
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2023-05-29 15:25:16 +0200

    Initial commit

---
+++ src/test/java/hs/aalen/urlaub/UrlaubApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.aalen.urlaub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlaubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
