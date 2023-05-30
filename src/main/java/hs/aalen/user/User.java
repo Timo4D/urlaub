71652bf8d21f8bbdaa1faa489aa55aefecb8d08d
Peter Ruhland <ruhland.peter@gmx.net>
2023-05-30 12:19:18 +0200

    first changes class Vacation.java and formatting

--- src/main/java/hs/aalen/user/User.java
+++ src/main/java/hs/aalen/user/User.java
@@ -2,68 +2,72 @@ package hs.aalen.user;
 
 //import needed for Date-datatype
 import java.sql.Date;
+
 public class User {
 
-    
-    private long id;
-    private String name;
-    private String surname;
-    private Date birthdate;
-    //mail-address could be helpful/ necessary for login reasons?
-    private String email;
+  private long id;
+  private String name;
+  private String surname;
+  private Date birthdate;
+  //mail-address could be helpful/ necessary for login reasons?
+  private String email;
 
-    //default constructor
-    public User(){
-    }
+  //default constructor
+  public User() {}
 
-    //constructor with variables
-    public User(long id, String name, String surname, Date birthdate, String email){
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.birthdate =birthdate;
-        this.email = email;
-    }
+  //constructor with variables
+  public User(
+    long id,
+    String name,
+    String surname,
+    Date birthdate,
+    String email
+  ) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.birthdate = birthdate;
+    this.email = email;
+  }
 
-    //Getters and Setters
-    public long getId() {
-        return id;
-    }
+  //Getters and Setters
+  public long getId() {
+    return id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getSurname() {
-        return surname;
-    }
+  public String getSurname() {
+    return surname;
+  }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
 
-    public Date getBirthdate() {
-        return birthdate;
-    }
+  public Date getBirthdate() {
+    return birthdate;
+  }
 
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
+  public void setBirthdate(Date birthdate) {
+    this.birthdate = birthdate;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
