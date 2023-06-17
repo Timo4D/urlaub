8aa24a6d1c7b04a39bb04c5cbff5cb2c95414dc9
robinh15 <128604237+robinh15@users.noreply.github.com>
2023-06-17 14:22:40 +0200

    Fixed Raing Method

--- src/main/java/hs/aalen/urlaub/rating/RatingService.java
+++ src/main/java/hs/aalen/urlaub/rating/RatingService.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hs.aalen.urlaub.member.Member;
+import hs.aalen.urlaub.member.MemberService;
 import hs.aalen.urlaub.vacationWish.VacationWish;
+import hs.aalen.urlaub.vacationWish.VacationWishService;
 
 @Service
 public class RatingService {
 
   @Autowired
   RatingRepository ratingRepository;
-
+  @Autowired
+  MemberService memberService;
+    @Autowired
+  VacationWishService vacationWishService;
 
 
   // This method can be used to get all the ratings given by a specific member
@@ -30,15 +35,18 @@ public class RatingService {
     return ratingRepository.findByMemberIdAndVacationWishId(member.getId(), vacationWish.getId()) != null;
   }
 
-  public void addRating(Rating rating) {
-    if (rating.getScore() < 1 || rating.getScore() > 10) {
-      throw new IllegalArgumentException("Score must be between 1 and 10");
+
+  public Rating createRating(Long memberId, Long vacationWishId, int score) {
+        Member member = memberService.getMember(memberId);  // replace with your method to get Member
+        VacationWish vacationWish = vacationWishService.getVacationWish(vacationWishId);  // replace with your method to get VacationWish
+
+        Rating rating = new Rating();
+        rating.setMember(member);
+        rating.setVacationWish(vacationWish);
+        rating.setScore(score);
+
+        return ratingRepository.save(rating);
     }
-    if (hasMemberRatedVacationWish(rating.getMember(), rating.getVacationWish())) {
-      throw new IllegalStateException("Member has already rated this vacation wish");
-    }
-    ratingRepository.save(rating);
-  }
 
   public Double getAverageRatingForVacationWish(Long vacationWishId) {
     return ratingRepository.findAverageRatingByVacationWishId(vacationWishId);
@@ -48,5 +56,5 @@ public class RatingService {
         return ratingRepository.findByMemberIdAndVacationWishId(memberId, vacationWishId)
             .orElseThrow(() -> new IllegalArgumentException("No rating found for the given member and vacation wish IDs"));
     }
-    
+
   }
