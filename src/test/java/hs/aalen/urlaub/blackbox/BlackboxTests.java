62c889d2053c2418032ed91c763a98af8b78f079
robinh15 <128604237+robinh15@users.noreply.github.com>
2023-06-21 14:45:23 +0200

    Test for login with invalid access code

--- src/test/java/hs/aalen/urlaub/blackbox/BlackboxTests.java
+++ src/test/java/hs/aalen/urlaub/blackbox/BlackboxTests.java
@@ -1,23 +1,39 @@
 package hs.aalen.urlaub.blackbox;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import hs.aalen.urlaub.member.Member;
 import hs.aalen.urlaub.member.MemberController;
 import hs.aalen.urlaub.member.MemberRepository;
+import hs.aalen.urlaub.member.MemberService;
 import hs.aalen.urlaub.security.SecurityController;
 import hs.aalen.urlaub.vacationWish.VacationWishController;
+import jakarta.transaction.Transactional;
+
 import java.sql.Date;
 import java.util.Optional;
+import java.util.Random;
+
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class BlackboxTests {
 
   @Autowired
@@ -102,11 +118,32 @@ public class BlackboxTests {
         .body("User registered successfully");
     }
   }
+ 
 
-  @AfterEach //delete created member/user after test
-  public void cleanup() {
-    if (createdId != null) {
-      memberController.deleteMember(createdId);
+ @Test // Test for login with invalid access code
+public void testInvalidUserLogin() {
+    String email = "invalid@example.com";
+    String password = "invalidpassword";
+
+    ModelAndView loginMav = securityController.login();
+    assertEquals("login", loginMav.getViewName());
+
+    ResponseEntity<String> response = loginUser(email, password);
+
+    assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    assertEquals("Invalid email or password", response.getBody());
+}
+
+// Methode für die Anmeldung eines Benutzers
+private ResponseEntity<String> loginUser(String email, String password) {
+    Optional<Member> existingMember = memberRepository.findByEmail(email);
+    if (existingMember.isPresent() && existingMember.get().getPassword().equals(password)) {
+        return ResponseEntity.status(HttpStatus.OK).body("Login successful");
+    } else {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
     }
-  }
+}
+
+
+
 }
